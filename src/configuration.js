678951c55c4dafb2e4d7612deb70bacbca5931ee
Ben <costantini.benjamin@gmail.com>
2020-10-04 12:31:22 +0200

    Update configuration.js

--- src/configuration.js
+++ src/configuration.js
@@ -2,9 +2,9 @@ const config = {
   header: "Header",
   headerBackground: "#ff8ba7",
   bodyBackground: "#ffc6c7",
-  title: "Title",
-  subTitle: "Sub Title",
-  airtable: "https://airtable.com/embed/shrdi24ocwoEeXx6d?backgroundColor=red&viewControls=on",
+  title: "Coffee with Sesame",
+  subTitle: "Member Hub",
+  airtable: "https://airtable.com/embed/shrDHQN6QyBxrcfTu?backgroundColor=red&viewControls=on",
   googleAnalytics: "UA-XXXXXXXX-X"
 }
 
